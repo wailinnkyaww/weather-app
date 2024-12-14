f2024370c4d31cb694bb5b0d7a94f44bc006c162
wailinnkyaww <wlin76478@gmail.com>
2024-12-14 12:40:41 +0630

    start api

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_iconn from "../assets/cloud.png";
+import drizzle_iconn from "../assets/drizzle.png";
+import humidity_iconn from "../assets/humidity.png";
+import rain_iconn from "../assets/rain.png";
+import snow_iconn from "../assets/snow.png";
+import wind_iconn from "../assets/wind.png";
 const Weather = () => {
+  /// fetch API key From online ///
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=
+        8e3f9a6d5629ee879c3ce859ea285de6
+      `;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    search("London");
+  }, []);
   return (
     <div className="weather">
       <div className="search-bar">
         <input type="text" placeholder="Enter Country" />
-        <img src="" alt="" />
+        <img src={search_icon} alt="" />
       </div>
-      <img src="" alt="" className="weather-icon" />
-      <p className="temperature">20'</p>
-      <p className="Location">London</p>
+      <img src={clear_icon} alt="" className="weather-icon" />
+      <p className="temperature">20'C</p>
+      <p className="location">London</p>
       <div className="weather-data">
         <div className="col">
-          <img src="" alt="" />
+          <img src={humidity_iconn} alt="" />
           <div>
             <p>90 %</p>
-            <span>HUmidity</span>
+            <span>Humidity</span>
           </div>
         </div>
         <div className="col">
-          <img src="" alt="" />
+          <img src={wind_iconn} alt="" />
           <div>
-            <p>3.6</p>
+            <p>3.6 km/h</p>
             <span>Wind Speed</span>
           </div>
         </div>
